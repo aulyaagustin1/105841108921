5a470efd4394390b013aae509e4b43a044a7e2f7
aulyaagustin1 <105841108921@student.unismuh.ac.id>
2024-05-31 22:06:01 +0800

    tugas Lab

--- App.js
+++ App.js
@@ -1,32 +1,86 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 const App = () => {
-  return (
-    <View style={{flex: 1}}>
-    <View style={{flex: 1, flexDirection: "row", backgroundColor: "white"}}>
-      <View style={{flex: 1, backgroundColor: "yellow"}}></View>
-    
-    <View style={{
-      flex: 1,
-      backgroundColor: "green"
-    
-    }}>
-    </View>
-    </View>
-    <View style={{
-      flex: 1,
-      backgroundColor: "red"
-    
-    }}>
-    </View>
-    <View style={{
-      flex: 1,
-      backgroundColor: "blue"
-    
-    }}>
-    </View>
-    </View>
-  )
+    return (
+        <View style={{flex:1}}>
+            <View style={{   
+                backgroundColor: '#E5E4E2',
+                top: 0,
+                bottom: 0,
+                height: 60,
+                justifyContent: 'center',
+            }}>
+                <Text style={{
+                    fontSize:30,
+                    color: 'green',
+                    textAlign: 'center',
+                    fontWeight: 'bold'
+                }}>
+                    aulyaStore
+                </Text>
+            </View>
+            <View style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+                top: '10%'
+            }}>
+                <Image source={require('./assets/cat.jpeg')} style={{
+                    height: '100%',
+                    width: '80%',
+                    resizeMode: 'contain',
+                }} />
+            </View>
+            <View style={{
+                flex: 1,
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'flex-end',
+            }}>
+                <View style={{
+                    backgroundColor: 'blue',
+                    height: 60,
+                    width: 120,
+                    justifyContent: 'center',
+                    borderRadius: 10,
+                    bottom: 40,
+                    right: 10
+                }}>
+                    <Text style={{
+                        color: 'white',
+                        fontSize: 20,
+                        fontWeight: 'bold',
+                        alignItems: 'center',
+                        textAlign: 'center'
+                    }}> 
+                    Login 
+                    </Text>
+                </View>
+                <View style={{
+                    backgroundColor: 'green',
+                    height: 60,
+                    width: 120,
+                    justifyContent: 'center',
+                    borderRadius: 10,
+                    bottom: 40,
+                    left: 10
+                
+                }}>
+                    <Text style={{
+                        color: 'white',
+                        fontSize: 20,
+                        fontWeight: 'bold',
+                        alignItems: 'center',
+                        textAlign: 'center'
+                    }}> 
+                    Register 
+                    </Text>
+                </View>
+            </View>
+        </View>
+
+    );
 }
+
 export default App;
